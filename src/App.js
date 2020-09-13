c82d66e0620d5fd50ddc9845d31fe4dc638e0d60
AnandaDwiPrayoga <Prayogaananda04@gmail.com>
2020-09-13 16:44:39 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './App.css';
 
-// import components
-import Nav from './components/Nav';
-import Tweets from './components/Tweets';
+const App = () => {
+  //variabel in index 0 is the value that always changed
+  //variabel in index 1 is the function that to change the variabel
+  const [ index, addIndex ] = useState(0);
+  const [ isDarkMode, changeMode ] = useState(false);
 
-const App = () => (
-  <div className="App"> 
-    <h1>Hello React</h1>
-    <div className="home">
-      <Nav />
-      <Tweets />
-    </div>
-  </div>
-);
+  const counterAdd = () => {
+    addIndex(prevIndex => prevIndex+1);
+    // OR addIndex(index + 1);
+  };
+
+  const checkboxClicked = () => changeMode(!isDarkMode);
+
+  let AppClass = 'App';
+  if(isDarkMode) AppClass += ' dark';
+
+  return (
+    <div className={AppClass} > 
+      <h1>Hello React</h1>
+      <p>Total clicked  = {index}</p>
+      <button onClick={counterAdd}>Click To Increament</button>
+      <div className="input-darkmode">
+          <input onClick={checkboxClicked} type="checkbox" id="darkmode"/> <label htmlFor="darkmode">Dark Mode</label>
+      </div>
+   </div>
+  );
+};
 
 export default App;
-
 
