00d5878d3a9512755433d4e045d85496ae5165f8
Ananda Dwi Prayoga <prayogaananda04@gmail.com>
2020-12-10 17:22:39 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+import Base from './components/Base';
+import Toppings from './components/Toppings';
+import Order from './components/Order';
 
-// import components
-import Nav from './components/Nav';
-import Tweets from './components/Tweets';
+function App() {
+  const [pizza, setPizza] = useState({ base: "", toppings: [] });
 
-const App = () => (
-  <div className="App"> 
-    <h1>Hello React</h1>
-    <div className="home">
-      <Nav />
-      <Tweets />
-    </div>
-  </div>
-);
+  const addBase = (base) => {
+    setPizza({ ...pizza, base })
+  }
+  
+  const addTopping = (topping) => {
+    let newToppings;
+    if(!pizza.toppings.includes(topping)){
+      newToppings = [...pizza.toppings, topping];
+    } else {
+      newToppings = pizza.toppings.filter(item => item !== topping);
+    }
+    setPizza({ ...pizza, toppings: newToppings });
+  }
+
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route path="/base">
+          <Base addBase={addBase} pizza={pizza} />
+        </Route>
+        <Route path="/toppings">
+          <Toppings addTopping={addTopping} pizza={pizza} />
+        </Route>
+        <Route path="/order">
+          <Order pizza={pizza} />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </>
+  );
+}
 
 export default App;
-
-
